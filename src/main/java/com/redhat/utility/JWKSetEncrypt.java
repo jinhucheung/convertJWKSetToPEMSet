9a62182dc38ee5cccfa472583ee7bde19a0cfbdf
jimcheung <hi.jinhu.zhang@gmail.com>
2020-03-17 00:21:31 +0800

    feat: fix jmeter

--- src/main/java/com/redhat/utility/JWKSetEncrypt.java
+++ src/main/java/com/redhat/utility/JWKSetEncrypt.java
@@ -1,31 +1,32 @@
 package com.redhat.utility;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class JWKSetEncrypt {
-    public static List<String> encrypt(String jwksetUrl, String...rawTexts) throws Exception {
+    public static String[] encrypt(String jwksetUrl, String[] rawTexts) throws Exception {
         String publicKeyJson = ObtainPublicKey.fetch(jwksetUrl);
         JsonObject publicKeyObject = JsonParser.parseString(publicKeyJson).getAsJsonObject();
         String publicKey = publicKeyObject.get("publicKeys").getAsJsonArray().get(0).getAsJsonObject().get("publicKey").getAsString();
 
-        List<String> encryptedTexts = new ArrayList<String>();
+        String[] encryptedTexts = new String[rawTexts.length];
 
-        for (String rawText : rawTexts) {
-            String encryptedText = RSA.encryptFromPublicKeyString(rawText, publicKey);
-            encryptedTexts.add(encryptedText);
+        for(int i = 0; i < rawTexts.length; i++) {
+            String encryptedText = RSA.encryptFromPublicKeyString(rawTexts[i], publicKey);
+            encryptedTexts[i] = encryptedText;
         }
 
         return encryptedTexts;
     }
 
+    public static String ping() {
+        return "Ping...";
+    }
 
     public static void main(String[] args) throws Exception {
-        List<String> encryptedTexts = JWKSetEncrypt.encrypt(args[0], Arrays.copyOfRange(args, 1, args.length));
+        String[] encryptedTexts = JWKSetEncrypt.encrypt(args[0], Arrays.copyOfRange(args, 1, args.length));
 
         for (String encryptedText : encryptedTexts) {
             System.out.println(encryptedText);
